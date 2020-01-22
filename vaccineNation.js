6afb245b28a5fc2be638673876fd291d3d1eba4c
Steven Hejnas <shejnas91@gmail.com>
2020-01-22 14:07:13 -0500

    Unknown commit

--- vaccineNation.js
+++ vaccineNation.js
@@ -1,51 +1,40 @@
-
-
-console.log('Hello?')
-
 function vaccineList(age, status, month) {
      let vaccArr = [];
 
-     function vaccFinder(val) {
-         
+        function vaccFinder(val) { 
         switch (val) {
-                case '8 weeks' : 
-                vaccArr.push(['fiveInOne', 'pneumococcal', 'rotavirus', 'meningitisB'])
+                case '8 weeks' :
+                vaccArr.push(['fiveInOne', 'pneumococcal', 'rotavirus', 'meningitisB']);
                     break;
                 case '12 weeks':
-                vaccArr.push(['fiveInOne','rotavirus'])
+                vaccArr.push(['fiveInOne','rotavirus']);
                     break;
                 case '16 weeks':
-                vaccArr.push(['fiveInOne', 'pneumococcal', 'meningitisB'])
+                vaccArr.push(['fiveInOne', 'pneumococcal', 'meningitisB']);
                     break;
                 case '12 months':
-                vaccArr.push(['meningitisB', 'hibMenC', 'measlesMumpsRubella'])
+                vaccArr.push(['meningitisB', 'hibMenC', 'measlesMumpsRubella']);
                     break;
                 case '40 months':
-                vaccArr.push(['measlesMumpsRubella', 'preSchoolBooster'])
+                vaccArr.push(['measlesMumpsRubella', 'preSchoolBooster']);
                     break;
                 case 'september':
                 case 'october':
                 case 'november':
-                vaccArr.push(['offer fluVaccine'])
+                vaccArr.push(['offer fluVaccine']);
                     break;
-        
-                default:
+                    default:
                     break;
         }
     }
 vaccFinder(age);
 vaccFinder(status);
 vaccFinder(month);
-//flatten array , need to sort array alphabetically, also removing repeated vaccs
-console.log(vaccArr)
 
-console.log('Hello?')
-
-
-
-
-return vaccArr;
-
+//flatten array , sort array alphabetically, also removing repeat
+//vaccArr = vaccArr.flat().sort(); codeWars doesnt like .flat
+vaccArr = [].concat.apply([], vaccArr).sort();
+return [...new Set(vaccArr)];
 }
 
 
@@ -57,4 +46,4 @@ console.log(vaccineList('12 months','16 weeks','june'));
 //['fiveInOne', 'hibMenC', 'measlesMumpsRubella', 'meningitisB', 'pneumococcal'],
 
 console.log(vaccineList('40 months','12 months','october'));
-// ['hibMenC','measlesMumpsRubella','meningitisB','offer fluVaccine','preSchoolBooster'],
+// ['hibMenC','measlesMumpsRubella','meningitisB','offer fluVaccine','preSchoolBooster']
